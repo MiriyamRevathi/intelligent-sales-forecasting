63259b763b205eb2364c15d25e253ba3d8e9558b
MiriyamRevathi <miriyamrevathi4@gmail.com>
2026-06-11 19:26:59 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+        use_reloader=False
+    )
